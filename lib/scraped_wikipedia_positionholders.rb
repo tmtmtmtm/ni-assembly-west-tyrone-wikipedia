f09e621fcf00ccb9227a986e721020ae0aa186d7
Tony Bowden <tony@tmtm.com>
2020-08-13 14:30:26 +0100

    add generic code

---
+++ lib/scraped_wikipedia_positionholders.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Scraped
+  module Wikipedia
+    # An extension to Scraped to represent a CSV version of a Wikipedia page
+    class PositionHolders
+      def initialize(confighash)
+        @url, @klass = confighash.to_a.first
+      end
+
+      def rows
+        @rows = scraper.rows
+      end
+
+      def to_csv
+        fields.to_csv + rows.map { |row| row.values.to_csv }.join
+      end
+
+      private
+
+      attr_reader :url, :klass
+
+      def scraper
+        @scraper ||= klass.new(response: Scraped::Request.new(url: url).response)
+      end
+
+      def fields
+        raise "No results" if rows.empty?
+
+        rows[1].keys
+      end
+    end
+  end
+end
